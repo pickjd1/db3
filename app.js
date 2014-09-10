1efacb70147c12e149968bac653cceb8549281c2
Joel Pickworth <pickjd1@gmail.com>
2014-09-10 03:31:06 +0000

    Splatter Client not working

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
 (function() { // We use this anonymous function to create a closure.
 
-	var app = angular.module('splatter-web', []);
+	var app = angular.module('splatter-web', ['ngResource']);
+
+	app.factory('User', function($resource)
+	{
+		return $resource('http://localhost/api/users/:id.json', {id: '@id'},
+		{'update': {method:'PUT}} );
+	});
 
 
-	app.controller('UserController', function() {
+	app.controller('UserController', function(User) {
         // add your user code below
-
-
-
-
+	this.data = {};
+	this.ulist = User.query();
+	this.getUser = function()
+	{
+		return user.get({this.data.id});
+	};
+//	this.feed = feed;
 	// add your user code above	
 	});
 
 
         // add your form controller below
+	this.createUser = function()
+	{
+		u = new User();
+		u.name = this.data.cname;
+		u.email = this.data.cemail;
+		u.password = this.data.cpassword;
+		u.blurb = this.data.cblurb;
+		u.$save();
+		this.data = {};
+	};
 
+	this.login = function()
+	{
+		this.loggedin_user = this.getUser(this.data.id);
+		this.data = {};
+	};
 
+	this.updateUser = function()
+	{
+		this.loggedin_user.name = this.data.name;
+		this.loggedin_user.email = this.data.email;
+		this.loggedin_user.blurb = this.data.blurb;
+		this.loggedin_user.$update();
+		this.data = {};
+	};
+
+	this.deleteUser = function()
+	{
+		User.delete({id: this.data.delid});
+	};
         // add your form controller above
 
 	// mock data
-        var u1 = {
+/*       var u1 = {
           id: 1,
           name: "Jane Doe",
           email: "jane@doe.com",
@@ -30,9 +67,18 @@
           id: 2,
 	  name: "Bob Smith",
 	  email: "bob@smith.org"
-	};
+	};*/
 
-       var feed = [
+	app.controller("UpdateFormController", function() {
+		this.data = {};
+		this.updateUser = function(user) {
+			user.u.name = this.data.name;
+			this.data = {}; //clears the form
+			}
+	});
+
+
+/*       var feed = [
          {
 	   id: 3,
            user_id: 2,
@@ -51,5 +97,5 @@
            body: "This is Bob's first splatt.",
 	   created_at: "2014-08-16T10:25:00Z"
 	 }
-       ];
+       ];*/
 })();
