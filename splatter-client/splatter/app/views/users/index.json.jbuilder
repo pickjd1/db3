3185e3b5c5f42d6294e9d7bc43fbca5d70979545
Joel Pickworth <pickjd1@gmail.com>
2014-09-12 02:17:34 +0000

    Remove Splatter-client

--- splatter-client/splatter/app/views/users/index.json.jbuilder
+++
@@ -1,6 +0,0 @@
-json.users @users do |user|
-  json.name user.name
-  json.email user.email
-  json.id user.id
-  json.blurb user.blurb  
-end
--- splatter-client/splatter/db/migrate/20140805225918_create_follows.rb
+++
@@ -1,8 +0,0 @@
-class CreateFollows < ActiveRecord::Migration
-  def change
-    create_table :follows do |t|
-     t.integer :follower_id, index: true
-     t.integer :followed_id, index: true
-   end
-  end
-end
